592036430c7d8992a6718d875e42220ec050c742
Koustubh-lapate <koustubh.lapate@gmail.com>
2024-12-17 17:19:37 +0530

    Hero section completed

--- frontend/src/sections/Hero.tsx
+++ frontend/src/sections/Hero.tsx
@@ -1,3 +1,83 @@
+import proImage from '@/assets/images/programmer.png';
+import Image from 'next/image';
+import ArrowDown from '@/assets/icons/arrow-down.svg';
+import grainImage from '@/assets/images/grain.jpg';
+import StarIcon from '@/assets/icons/star.svg';
+import { StarOrbit } from '@/components/StarOrbit';
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className='py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip'>
+      
+      <div className='absolute inset-0 [mask-image:linear-gradient(to_bottom, transparent, black_10%, black_70%, transparent)]'>
+        <div className='absolute inset-0 -z-30 opacity-5' style={{
+          backgroundImage: `url(${grainImage.src})`
+        }}>
+        </div>
+
+        <div className='absolute inset-0 size-[620px] border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-emerald-300/5 shadow-[0_0_80px_inset] shadow-emerald-300/5'></div>
+        <div className='absolute inset-0 size-[820px] border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-emerald-300/5 shadow-[0_0_80px_inset] shadow-emerald-300/5'></div>
+        <div className='absolute inset-0 size-[1020px] border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-emerald-300/5 shadow-[0_0_80px_inset] shadow-emerald-300/5'></div>
+        <div className='absolute inset-0 size-[1220px] border-2 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-emerald-300/5 shadow-[0_0_80px_inset] shadow-emerald-300/5'></div>
+
+        <StarOrbit size={800} rotation={-72}>
+          <StarIcon className="size-28 text-emerald-300" />
+        </StarOrbit>
+
+        <StarOrbit size={550} rotation={20}>
+          <StarIcon className="size-12 text-emerald-300" />
+        </StarOrbit>
+
+        <StarOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </StarOrbit>
+
+        <StarOrbit size={900} rotation={120}>
+          <StarIcon className="size-20 text-emerald-300" />
+        </StarOrbit>
+      </div>
+
+      <div className='container'>
+        
+        <div className='flex flex-col items-center'>
+          <Image 
+          src={proImage} 
+          className='size-[100px]'
+          alt='Person coding on his laptop' />
+          
+          <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
+            <div className='bg-green-500 size-2.5 rounded-full'></div>
+            <div className='text-sm font-medium'>Available for new Projects</div>
+          </div>
+        </div>
+
+        <div className='max-w-lg mx-auto'>
+          
+          <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>
+            Building Exceptional Web Applications
+          </h1>
+
+          <p className='mt-4 text-center text-white/60 md:text-lg'>
+            I specialize in creating tailored web solutions that meet client needs, ensuring seamless performance and engaging designs. Let's build something exceptional together!
+          </p>
+
+        </div>
+
+        <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
+
+          <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
+            <span className='font-semibold'>Explore My Work</span>
+            <ArrowDown className="size-4" />
+          </button>
+
+          <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl'>
+            <span>👋</span>
+            <span>Let's Connect</span>
+          </button>
+
+        </div>
+
+      </div>
+    </div>
+  );
 };
